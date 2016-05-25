b0b81ec1a7552787eee1373b69c20b51c84425d7
Exaltia <ludovic.dsj@protheine.com>
2016-05-25 23:49:21 +0200

    Fichiers inutiles supprimés!

--- mptest1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import time
-class Mptest1:
-	def mptest1(self, ):
-		i = 0
-		#while i < 10:
-		print "I'm Process 1"
-		time.sleep(1)
-		i = i + 1
--- mptest2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import time
-class Mptest2:
-	def mptest2(self, ):
-		i = 0
-		#while i < 10:
-		print "I'm Process 2"
-		time.sleep(2)
-		i = i + 1
