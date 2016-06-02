e95a68892f3f24422eadbd497b9a154ca0fa0943
Exaltia <ludovic.dsj@protheine.com>
2016-06-02 10:04:25 +0200

    Added temporary code for memory leak troubleshooting

--- threadedscan.py
+++ threadedscan.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+#Scan, scan, scan, and more scan, I WANT FRIENDS!
 from Queue import Queue
 import time, socket, sys, time, random, os
 class Testeur():
@@ -7,17 +8,17 @@ class Testeur():
 		BUFF = 1000
 		MESSAGE = 'WHOAREU'
 		message = ''
-		firstgroup = []
-		secondgroup = []
-		thirdgroup = []
-		fourthgroup = []
+		# firstgroup = []
+		# secondgroup = []
+		# thirdgroup = []
+		# fourthgroup = []
 		bb = []
 		nodetable = {}
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.settimeout(0.65)
-		for num in range (range1, range2):
+		#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		for num in xrange (range1, range2):
 			host = myip + '.' + str(num)
-			#print host
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.settimeout(0.65)
 			#ADDR = (host, self.PORT)
 			try:
 				#print 'je try'
@@ -27,12 +28,13 @@ class Testeur():
 				#print 'I connect'
 				lenmsg = len(MESSAGE)
 				lenmsg = str(lenmsg)
+				print len(lenmsg)
 				protomsg = lenmsg + ','+ '' + ',' + MESSAGE #client Wait for the following message : "messagelenght,message"
 				s.send(protomsg)
 				#print 'i send'
 				packet = s.recv(BUFF)
-				if 'MIAOU' in packet:
-					#print 'Found node ' + packet + ' at ' + str(host)
+				#print len(packet)
+				if 'MIAOU' in packet: #Check if it's one of our node by sending miaou
 					packet = packet.split(',' ,1)
 					packet = str(packet[1])
 					nodetable = {packet, host}
@@ -46,11 +48,21 @@ class Testeur():
 			except:
 				#print 'host' + host +  'except', sys.exc_info()
 				#print 'je vais close aussi'
-				s.close()
 				time.sleep(0.05)
+				#s.shutdown(socket.SHUT_RDWR)
+				#print len(nodetable)
 					#time.sleep(0.1)
 					#print bb
 				#print 'Pouet'
 			#print 'Going to out_q'
 			out_q.put(nodetable)
+			# del host
+			# del lenmsg
+			# del MESSAGE
+			# del protomsg
+			# del bb
+			# del nodetable
+		#print 'fini'
+		#s.shutdown(socket.SHUT_RDWR)
 q = Queue()
+
